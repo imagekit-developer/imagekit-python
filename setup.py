55b562de131a57eaa32cfa772056f301dfe9e5bc
Akshay Katyal <akshay@mindship.io>
2020-01-20 23:30:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="imagekitio",
-    version="2.0.0",
+    name="Imagekit",
+    version="2.0.1",
     description="Python wrapper for the ImageKit API",
     long_description=long_description,
     long_description_content_type="text/markdown",
