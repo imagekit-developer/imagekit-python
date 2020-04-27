caf766be0333343294eb0645923c7b851dfe2675
Manu Chaudhary <manuchaudhary@Manus-MacBook-Pro.local>
2020-04-27 11:24:25 +0530

    deleting temp file

--- sample/test.py
+++
@@ -1,2 +0,0 @@
-a = https://ik.imagekit.io/pshbwfiho/tr:h-300,w-400/testing-python-folder/testing_upload_binary_signed_private_D15-T6ji5.jpg?v=123&ik-sdk-version=python-2.2.4&ik-t=9999999999&ik-s=abef9e9f3115ad0b7fad82fba2c16ed6144c99c6
-
