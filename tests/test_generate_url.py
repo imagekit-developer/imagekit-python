4a766affb74edaa0a38be57ffd663fee25151826
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2024-01-31 05:04:49 +0530

    added effect_shadow and effect_gradient parameter

--- tests/test_generate_url.py
+++ tests/test_generate_url.py
@@ -364,32 +364,6 @@ class TestGenerateURL(unittest.TestCase):
                     "rotation": 90,
                     "blur": 10,
                     "named": "some_name",
-                    "overlay_image": "/folder/file.jpg",  # leading slash case
-                    "overlay_image_aspect_ratio": "4:3",
-                    "overlay_image_background": "0F0F0F",
-                    "overlay_image_border": "10_0F0F0F",
-                    "overlay_image_dpr": 2,
-                    "overlay_image_quality": 50,
-                    "overlay_image_cropping": "force",
-                    "overlay_image_trim": False,
-                    "overlay_x": 35,
-                    "overlay_y": 35,
-                    "overlay_focus": "bottom",
-                    "overlay_height": 20,
-                    "overlay_width": 20,
-                    "overlay_text": "two words",
-                    "overlay_text_font_size": 20,
-                    "overlay_text_font_family": "Open Sans",
-                    "overlay_text_color": "00FFFF",
-                    "overlay_text_transparency": 5,
-                    "overlay_text_typography": "b",
-                    "overlay_background": "00AAFF55",
-                    "overlay_text_encoded": "b3ZlcmxheSBtYWRlIGVhc3k%3D",
-                    "overlay_text_width": 50,
-                    "overlay_text_background": "00AAFF55",
-                    "overlay_text_padding": 40,
-                    "overlay_text_inner_alignment": "left",
-                    "overlay_radius": 10,
                     "progressive": "true",
                     "lossless": "true",
                     "trim": 5,
@@ -401,6 +375,8 @@ class TestGenerateURL(unittest.TestCase):
                     "effect_usm": "2-2-0.8-0.024",
                     "effect_contrast": "true",
                     "effect_gray": "true",
+                    "effect_shadow": "bl-15_st-40_x-10_y-N5",
+                    "effect_gradient": "from-red_to-white",
                     "original": True,  # Boolean handling
                     "raw": "w-200,h-200",
                 }
@@ -410,11 +386,8 @@ class TestGenerateURL(unittest.TestCase):
         self.assertEqual(
             url,
             "https://test-domain.com/test-endpoint/tr:h-300,w-400,ar-4-3,q-40,c-force,cm-extract,fo-left,f-jpeg,r-50,"
-            "bg-A94D34,b-5-A94D34,rt-90,bl-10,n-some_name,oi-folder@@file.jpg,oiar-4:3,oibg-0F0F0F,oib-10_0F0F0F,"
-            "oidpr-2,oiq-50,oic-force,oit-false,ox-35,oy-35,ofo-bottom,oh-20,ow-20,ot-two words,ots-20,otf-Open Sans,"
-            "otc-00FFFF,oa-5,ott-b,obg-00AAFF55,ote-b3ZlcmxheSBtYWRlIGVhc3k%3D,otw-50,otbg-00AAFF55,otp-40,otia-left,"
-            "or-10,pr-true,lo-true,t-5,md-true,cp-true,di-folder@@file.jpg,dpr-3,e-sharpen-10,e-usm-2-2-0.8-0.024,"
-            "e-contrast-true,e-grayscale-true,orig-true,w-200,h-200/test_path.jpg",
+            "bg-A94D34,b-5-A94D34,rt-90,bl-10,n-some_name,pr-true,lo-true,t-5,md-true,cp-true,di-folder@@file.jpg,dpr-3,e-sharpen-10,e-usm-2-2-0.8-0.024,"
+            "e-contrast-true,e-grayscale-true,e-shadow-bl-15_st-40_x-10_y-N5,e-gradient-from-red_to-white,orig-true,w-200,h-200/test_path.jpg",
         )
 
     def test_get_signature_with_100_expire_seconds(self):
