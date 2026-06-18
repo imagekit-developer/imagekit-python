a1080bed6a778947407da110b0e3ad8b21547b7e
Manu Chaudhary <manu@imagekit.io>
2026-06-18 12:36:17 +0530

    seal

--- src/imagekitio/types/shared/transformation.py
+++ src/imagekitio/types/shared/transformation.py
@@ -144,7 +144,7 @@ class Transformation(BaseModel):
 
     - `co-color` - Color to apply (e.g., `red`, `blue`, `FF0022`). Default is gray
       color.
-    - `in-intensity` - Intensity of the color (0-100). Default is 35. See
+    - `in-intensity` - Intensity of the color (0-100). Default is 100. See
       [Colorize](https://imagekit.io/docs/effects-and-enhancements#colorize---e-colorize).
     """
 
--- src/imagekitio/types/shared_params/transformation.py
+++ src/imagekitio/types/shared_params/transformation.py
@@ -142,7 +142,7 @@ class Transformation(TypedDict, total=False):
 
     - `co-color` - Color to apply (e.g., `red`, `blue`, `FF0022`). Default is gray
       color.
-    - `in-intensity` - Intensity of the color (0-100). Default is 35. See
+    - `in-intensity` - Intensity of the color (0-100). Default is 100. See
       [Colorize](https://imagekit.io/docs/effects-and-enhancements#colorize---e-colorize).
     """
 
